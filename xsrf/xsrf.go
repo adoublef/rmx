e9c84d8d4a2e04f5188a6a81e67ec0dac4fd7f46
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-11-17 04:03:57 +0000

    .

---
+++ xsrf/xsrf.go
@@ -0,0 +1,6 @@
+package xsrf
+
+// import xsrf "golang.org/x/net/xsrftoken"
+
+// tok := xsrf.Generate(hmacSecret, s.setSession(w, r), "")
+// 	ok := xsrf.Valid(r.PostFormValue("_xsrf"), hmacSecret, s.getSession(w, r), "")
